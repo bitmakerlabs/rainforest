c4f48f27eb928763f4f509fee6e1bc19fb7d6be4
Julie Hache <elektrophile@gmail.com>
2015-02-08 21:53:03 -0500

    Install Factory Girl

--- Gemfile
+++ Gemfile
@@ -43,5 +43,7 @@ group :development, :test do
   gem 'spring'
 
   gem 'rspec-rails'
+
+  gem 'factory_girl_rails'
 end
 
---
+++ spec/factories/products.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :product do
+    name "My Product"
+    description "My Product Description"
+    price_in_cents 100
+  end
+end
