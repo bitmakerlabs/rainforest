cc961f34a75fdd3753ea6d70aefe84b18cde527b
NatalieBlack <natalievictoriablack@gmail.com>
2016-05-29 14:02:23 -0400

    create empty rails project

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,4 @@
 Rails.application.routes.draw do
-  resources :products do
-    resources :reviews, only: [:show, :create, :destroy]
-  end
-
-  resources :users, only: [:new, :create]
-  resources :sessions, only: [:new, :create, :destroy]
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
